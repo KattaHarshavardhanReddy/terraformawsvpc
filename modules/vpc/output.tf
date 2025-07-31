3a643f3a68d1d05aefddc1e61e20face82ec289a
KattaHarshavardhanReddy <katta.harshareddy@gmail.com>
2025-07-31 23:07:44 +0530

    terraform

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -2,18 +2,18 @@
 #     value = data.aws_availability_zones.available
 # }
 
-# output "aws_vpc" {
-#     value = aws_vpc.main.id
-# }
+output "aws_vpc" {
+    value = aws_vpc.main.id
+}
 
-# output "public_subnet_id's" {
-#     value = aws_subnet.public[*].id
-# }
+output "public_subnet_id's" {
+    value = aws_subnet.public[*].id
+}
 
-# output "private_subnet_id's" {
-#     value = aws_subnet.database[*].id
-# }
+output "private_subnet_id's" {
+    value = aws_subnet.database[*].id
+}
 
-# output "database_subnet_id's" {
-#     value = aws_subnet.database[*].id
-# }
+output "database_subnet_id's" {
+    value = aws_subnet.database[*].id
+}
